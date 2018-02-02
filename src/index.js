d8d03f062fa151a37db56dcce953f26bfa0e4f33
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-02 14:18:37 +0300

    Delete all files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './static/index.css';
-import App from './app/App';
-import registerServiceWorker from './tools/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
