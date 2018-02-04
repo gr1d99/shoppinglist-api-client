3f173a67c504c49c5c8f497a7133a841782c4c90
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-04 13:13:43 +0300

    [Feature]Add custom app messages

---
+++ src/containers/utils/defaults.js
@@ -0,0 +1,8 @@
+export const LOGGED_IN = 'Logged in successfully';
+export const LOGGED_OUT = 'Logged out successfully';
+export const ACCOUNT_CREATED = 'Account created successfully, log in with your username and password';
+
+export const SHOPPING_ITEM_CREATED = 'Item successfully created';
+export const SHOPPING_ITEM_UPDATED = 'Your item has been updated';
+export const SHOPPING_LIST_CREATED = 'Shopping list successfully created';
+export const SHOPPING_LIST_UPDATED = 'Shopping list successfully updated';
