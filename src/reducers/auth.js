4eaa59cf1fa202654cd77b29dafab5e51f5bf2f1
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-04 14:08:41 +0300

    [Feature]Update auth reducer to include user detail

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -3,7 +3,7 @@ import * as types  from "../actions/ActionTypes";
 let _auth = !(localStorage.getItem('apiKey') === null);
 
 const Authenticate = (
-    state={isAuthenticated: _auth}, action) => {
+    state={isAuthenticated: _auth, userDetail: null}, action) => {
     switch (action.type) {
         case types.LOGIN:
             return Object.assign({}, state, {
@@ -11,19 +11,27 @@ const Authenticate = (
             });
 
         case types.LOGIN_ERROR:
-            return {login_errors: action.payload}
+            return {login_errors: action.payload};
 
         case types.SIGNUP:
-            return {success_message: action.payload.data.message}
+            return {success_message: action.payload.data.message};
 
         case types.SIGNUP_ERROR:
-            return {signup_errors: action.payload}
+            return {signup_errors: action.payload};
 
         case types.LOGOUT:
             return state;
 
         case types.LOGOUT_ERROR:
             return {login_error: action.payload};
+
+        case types.USER_INFO:
+            return Object.assign({}, state, {
+                userDetail: action.payload.data.data
+            });
+
+        case types.USER_INFO_ERROR:
+            return action.payload;
 
         default:
             return state
